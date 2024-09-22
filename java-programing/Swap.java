ffa617debc9e4fa6b87dc81a95c2cced38187ee9
sonu86377 <64629662+sonu86377@users.noreply.github.com>
2024-09-22 20:54:48 +0530

    Add files via upload

---
+++ java-programing/Swap.java
@@ -0,0 +1,15 @@
+// 2. Write a Java program to interchange values of two variable without using any third variable?
+
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println("A = " + a + "\n B = " + b);
+    }
+    
+}
