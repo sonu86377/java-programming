536e485b9459a0b1c44c88be43639bf2d3a643d5
sonu86377 <sonu86377@gmail.com>
2024-10-05 11:53:46 +0530

    swap of two number using temp variable

---
+++ Swap1.java
@@ -0,0 +1,12 @@
+// Write a java program to swap two number using temp variable.
+public class Swap1 {
+    public static void main(String[] args){
+        int a = 100;
+        int b = 200;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = "+a + "\nb = " +b);
+    }
+    
+}
